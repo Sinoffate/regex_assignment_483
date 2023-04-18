29da07a35fda5e9c60f53bc6a4c1daed4b505e7d
Jered W <sinoffate@hotmail.com>
2023-04-18 16:49:14 -0700

    Completed initial versions of: - Date validation ------------------------------ - Added Unit tests for SSN Validation

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,7 @@ fn main() {
     io::stdin()
         .read_line(&mut email_input)
         .expect("Failed to read line");
+
     let email_input = email_input.trim();
     if validate_email(email_input) {
         println!("Valid email");
@@ -42,7 +43,6 @@ fn main() {
     }
 
     println!("Enter a class roster name (Last name, First name, MI):");
-
     let mut roster_name_input = String::new();
     io::stdin().read_line(&mut roster_name_input).expect("Failed to read input");
 
@@ -53,6 +53,16 @@ fn main() {
         println!("The class roster name is invalid.");
     }
 
+    println!("Enter a date (MM-DD-YYYY or MM/DD/YYYY):");
+    let mut date_input = String::new();
+    io::stdin().read_line(&mut date_input).expect("Failed to read input");
+
+    let date_input = date_input.trim();
+    if validate_date(date_input) {
+        println!("The date is valid.");
+    } else {
+        println!("The date is invalid.");
+    }
 }
 
 lazy_static! {
@@ -60,6 +70,7 @@ lazy_static! {
     static ref PHONE_NUMBER_REGEX: Regex = Regex::new(r"^\s*\(?(\d{3})\)?[-\s]?(\d{3})[-\s]?(\d{4})\s*$").unwrap();
     static ref EMAIL_REGEX: Regex = Regex::new(r"^(?i)[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$").unwrap();
     static ref NAME_ROSTER_REGEX: Regex = Regex::new(r"^(?P<last>[a-zA-Z]+),\s*(?P<first>[a-zA-Z]+)(,\s*(?P<middle>[a-zA-Z]))*$").unwrap();
+    static ref DATE_REGEX: Regex = Regex::new(r"^(?P<month>0[1-9]|1[0-2])[-/](?P<day>0[1-9]|[12]\d|3[01])[-/](?P<year>\d{4})$").unwrap();
 }
 
 fn get_ssn() -> String {
@@ -93,11 +104,6 @@ fn validate_ssn(ssn: &str) -> bool {
             return false;
         }
 
-        // Reserved for future use: 900-999
-        if area >= 900 {
-            return false;
-        }
-
         return true;
     }
     false
@@ -121,4 +127,91 @@ fn validate_email(email: &str) -> bool {
 
 fn validate_name_roster(name_roster: &str) -> bool {
     NAME_ROSTER_REGEX.is_match(name_roster)
+}
+
+fn is_leap_year(year: u16) -> bool {
+    (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)
+}
+
+fn validate_date(date: &str) -> bool {
+    if let Some(captures) = DATE_REGEX.captures(date) {
+        let month = captures.name("month").unwrap().as_str().parse::<u16>().unwrap();
+        let day = captures.name("day").unwrap().as_str().parse::<u16>().unwrap();
+        let year = captures.name("year").unwrap().as_str().parse::<u16>().unwrap();
+
+        let days_in_month = match month {
+            1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+            4 | 6 | 9 | 11 => 30,
+            2 if is_leap_year(year) => 29,
+            2 => 28,
+            _ => return false,
+        };
+
+        if day > 0 && day <= days_in_month {
+            return true;
+        }
+    }
+
+    false
+}
+
+
+
+
+
+// Unit tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_validate_ssn_with_dashes() {
+        assert!(validate_ssn("123-45-6789"));
+    }
+
+    #[test]
+    fn test_validate_ssn_with_spaces() {
+        assert!(validate_ssn("123 45 6789"));
+    }
+
+    #[test]
+    fn test_validate_ssn_with_dashes_and_spaces() {
+        assert!(validate_ssn("123-45 6789"));
+    }
+
+    #[test]
+    fn test_validate_ssn_with_spaces_and_dashes() {
+        assert!(validate_ssn("123 45-6789"));
+    }
+
+    #[test]
+    fn test_validate_ssn_with_invalid_area_number() {
+        assert!(!validate_ssn("734-45-6789"));
+    }
+
+    #[test]
+    fn test_validate_ssn_with_invalid_group_number() {
+        assert!(!validate_ssn("123-00-6789"));
+    }
+
+    #[test]
+    fn test_validate_ssn_with_invalid_serial_number() {
+        assert!(!validate_ssn("123-45-0000"));
+    }
+
+    #[test]
+    fn test_validate_ssn_with_invalid_area_number_666() {
+        assert!(!validate_ssn("666-45-6789"));
+    }
+
+    #[test]
+    fn test_validate_ssn_with_invalid_area_number_772() {
+        assert!(!validate_ssn("772-45-6789"));
+    }
+
+    #[test]
+    fn test_validate_ssn_with_invalid_area_number_800() {
+        assert!(!validate_ssn("800-45-6789"));
+    }
+
 }
